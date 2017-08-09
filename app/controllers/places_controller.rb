3667f4c1da6ab6cd3ab75cf86e602bc118d538f5
mdunnettstone <mdunnettstone@gmail.com>
2017-08-09 17:00:47 +0000

    CRUD functionality for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @places = Place.all.paginate(:page => params[:page], :per_page => 10)
+    @places = Place.all.paginate(:page => params[:page], :per_page => 4)
 
   end
 
@@ -12,6 +12,26 @@ class PlacesController < ApplicationController
 
   def create
     current_user.places.create(place_params)
+    redirect_to root_path
+  end
+
+  def show
+    @place = Place.find_by_id(params[:id])
+  end
+
+  def edit
+    @place = Place.find_by_id(params[:id])
+  end
+
+  def update
+    @place = Place.find_by_id(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to place_path
+  end
+
+  def destroy
+    @place = Place.find_by_id(params[:id])
+    @place.destroy
     redirect_to root_path
   end
 
